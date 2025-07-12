b6e6f8e6a2e1fb1a2d9962874dbf8006e7d15da1
Emmanuel Akabike <akabikeemmanuel@gmail.com>
2025-07-12 18:59:52 +0100

    Update 2-arguments.js: clean logic without length or var

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (!arg1) {
+  console.log("No argument");
+} else if (!arg2) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
